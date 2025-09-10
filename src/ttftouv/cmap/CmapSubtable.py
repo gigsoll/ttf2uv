763f40ee3100cb39492f4fbf70a745b2e2e7c75e
Anatoliy Dudar <gigsollt@gmail.com>
2025-09-10 16:16:57 +0300

    implemented Binary reader in cmap

--- src/ttftouv/cmap/CmapSubtable.py
+++ src/ttftouv/cmap/CmapSubtable.py
@@ -1,5 +1,14 @@
 class CmapSubtable:
-    def __init__(self, binary_data: bytes) -> None:
-        self.binary_data: bytes = binary_data
+    def __init__(
+        self,
+        platform_id: int,
+        platform_specific_id: int,
+        offset: int,
+        binary_data: bytes,
+    ) -> None:
+        self.platform_id = platform_id
+        self.platform_specific_id = platform_specific_id
+        self.offset = offset
+        self.binary_data: bytes = binary_data[offset:]
 
-    def map_character(self, character: bytes) -> int: ...
+    def map_character(self, character_code: int) -> int: ...
